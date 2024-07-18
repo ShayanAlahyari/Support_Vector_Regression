382498f3bb8a960d37ff09e1b70afd6478d0e62a
shayanalahyari <shayanforstock@gmail.com>
2024-07-18 21:43:00 +0330

    update

--- support_vector_machine.py
+++ support_vector_machine.py
@@ -2,13 +2,19 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVR
 
 # Load dataset
 dataset = pd.read_csv('Position_Salaries.csv')
 x = dataset.iloc[:, 1:-1].values  # Extracting independent variable (Position level)
-y = dataset.iloc[:, -1].values    # Extracting dependent variable (Salary)
+y = dataset.iloc[:, -1].values  # Extracting dependent variable (Salary)
 
-y = y.reshape(len(y),1)
+y = y.reshape(len(y), 1)
 
+sc_x = StandardScaler()
+sc_y = StandardScaler()
+x = sc_x.fit_transform(x)
+y = sc_y.fit_transform(y)
 
-sc = StandardScaler()
+regressor = SVR(kernel='rbf')
+regressor.fit(x, y)
