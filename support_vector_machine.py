0759d4cb486b69009239348b5ca5d47325387d19
shayanalahyari <shayanforstock@gmail.com>
2024-07-18 14:27:35 +0330

    update

--- support_vector_machine.py
+++ support_vector_machine.py
@@ -1,3 +1,9 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# Load dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+x = dataset.iloc[:, 1:-1].values  # Extracting independent variable (Position level)
+y = dataset.iloc[:, -1].values    # Extracting dependent variable (Salary)
